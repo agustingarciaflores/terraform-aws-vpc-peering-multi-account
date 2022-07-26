88b3aef1eaa9e99819053f66543f5c49dc8de60a
Agustín García Flores <aflores@anaconda.com>
2022-07-26 17:01:59 -0400

    Update fixtures

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,23 +1,24 @@
 provider "aws" {
-  alias  = "accpt"
+  alias  = "accepter"
   region = var.region
 }
 
 provider "aws" {
-  alias  = "req"
+  alias  = "requester"
   region = var.region
 }
 
 module "vpc_peering_cross_account" {
   source = "../../"
   providers = {
-    aws.accepter  = aws.accpt
-    aws.requester = aws.req
+    aws.accepter  = aws.accepter
+    aws.requester = aws.requester
   }
 
   requester_vpc_id                          = var.requester_vpc_id
   requester_allow_remote_vpc_dns_resolution = var.requester_allow_remote_vpc_dns_resolution
 
+  accepter_enabled                         = var.accepter_enabled
   accepter_vpc_id                          = var.accepter_vpc_id
   accepter_allow_remote_vpc_dns_resolution = var.accepter_allow_remote_vpc_dns_resolution
 
